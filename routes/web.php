a691b936ada9859078c519501525038f2ec8fd0e
Xanderificnl <xander@web-refinery.com>
2021-11-23 14:08:46 +0100

    Add the home route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::resource('article', ArticleController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
