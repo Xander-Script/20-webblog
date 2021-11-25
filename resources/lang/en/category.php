c71afd5211242242e8ecb70c1fcf1f5fb6c4f7c1
Xanderificnl <xander@web-refinery.com>
2021-11-25 08:41:52 +0100

    Add category translations

---
+++ resources/lang/en/category.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'information_line' => '{0} Nothing is filed in this category yet.|{1} One article is filed in this category.|{2} :articles articles written by :authors authors filed in this category.',
+];
