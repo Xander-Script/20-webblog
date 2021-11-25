03a1abfd7c17951b7c5efaa780c896dc72418508
Xanderificnl <xander@web-refinery.com>
2021-11-25 10:22:04 +0100

    Add number converter

--- config/app.php
+++ config/app.php
@@ -231,6 +231,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
+        'NumConvert' => 'HnhDigital\LaravelNumberConverter\Facade',
 
 
     ],
