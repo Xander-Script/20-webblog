4c8479c11460d85e3a3111ba531cf62444faa497
Xanderificnl <xander@web-refinery.com>
2021-11-26 15:28:23 +0100

    Clean up models

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,8 +2,13 @@
 
 namespace App\Models;
 
+use Database\Factories\ArticleFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
 /**
  * App\Models\Article
@@ -13,24 +18,24 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $body
  * @property int $user_id
  * @property int $draft
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  * @property int|null $category_id
- * @property-read \App\Models\Category|null $category
- * @property-read \App\Models\User $user
- * @method static \Database\Factories\ArticleFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Article newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Article newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Article query()
- * @method static \Illuminate\Database\Eloquent\Builder|Article whereBody($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Article whereCategoryId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Article whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Article whereDraft($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Article whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Article whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Article whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Article whereUserId($value)
- * @mixin \Eloquent
+ * @property-read Category|null $category
+ * @property-read User $user
+ * @method static ArticleFactory factory(...$parameters)
+ * @method static Builder|Article newModelQuery()
+ * @method static Builder|Article newQuery()
+ * @method static Builder|Article query()
+ * @method static Builder|Article whereBody($value)
+ * @method static Builder|Article whereCategoryId($value)
+ * @method static Builder|Article whereCreatedAt($value)
+ * @method static Builder|Article whereDraft($value)
+ * @method static Builder|Article whereId($value)
+ * @method static Builder|Article whereTitle($value)
+ * @method static Builder|Article whereUpdatedAt($value)
+ * @method static Builder|Article whereUserId($value)
+ * @mixin Eloquent
  */
 class Article extends Model
 {
@@ -40,17 +45,17 @@ class Article extends Model
         'user_id' => 1,
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class)->withDefault(function ($user, $post) {
             return User::find(1); // Guest User.
         });
     }
 
-    public function category()
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class)->withDefault(function ($category, $post) {
-            $category->name = 'Uncategorized';
+            $category = Category::find(1);
         });
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Database\Factories\CategoryFactory;
+use Eloquent;
 use HnhDigital\LaravelNumberConverter\Facade as NumConvert;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
 /**
  * App\Models\Category
@@ -12,42 +18,42 @@ use Illuminate\Database\Eloquent\Model;
  * @property int $id
  * @property string $name
  * @property int|null $categories_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  * @property string|null $description
  * @property int|null $article_count
  * @property int|null $author_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Article[] $articles
+ * @property-read Collection|Article[] $articles
  * @property-read int|null $articles_count
- * @method static \Database\Factories\CategoryFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Category newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Category newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Category query()
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereArticleCount($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereAuthorCount($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereCategoriesId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereUpdatedAt($value)
- * @mixin \Eloquent
+ * @method static CategoryFactory factory(...$parameters)
+ * @method static Builder|Category newModelQuery()
+ * @method static Builder|Category newQuery()
+ * @method static Builder|Category query()
+ * @method static Builder|Category whereArticleCount($value)
+ * @method static Builder|Category whereAuthorCount($value)
+ * @method static Builder|Category whereCategoriesId($value)
+ * @method static Builder|Category whereCreatedAt($value)
+ * @method static Builder|Category whereDescription($value)
+ * @method static Builder|Category whereId($value)
+ * @method static Builder|Category whereName($value)
+ * @method static Builder|Category whereUpdatedAt($value)
+ * @mixin Eloquent
  */
 class Category extends Model
 {
     use HasFactory;
 
-    public function convertArticleCountToWords()
+    public function convertArticleCountToWords(): string
     {
         return NumConvert::word($this->article_count);
     }
 
-    public function convertAuthorCountToWords()
+    public function convertAuthorCountToWords(): string
     {
         return NumConvert::word($this->author_count);
     }
 
-    public function articles()
+    public function articles(): HasMany
     {
         return $this->hasMany(Article::class);
     }
