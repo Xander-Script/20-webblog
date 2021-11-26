ec28aee2c591b965fd32aaf45b0fcc662cc0f399
Xanderificnl <xander@web-refinery.com>
2021-11-26 16:38:25 +0100

    Initial HTTP tests for the Article Resource

---
+++ tests/Feature/HTTPArticleResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Article;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HTTPArticleResource extends TestCase
+{
+    public function testSingularArticleRouteReturnsPermanentRedirectStatusCode()
+    {
+        $redirect = $this->get('/article');
+        $redirect->assertStatus(308);
+        $redirect->assertRedirect('/articles');
+    }
+
+    public function testArticleOverviewPageReturnsOkStatusCode()
+    {
+        $response = $this->get('/articles');
+        $response->assertStatus(200);
+    }
+
+    public function testSingleArticlePageReturnsOkStatusCode()
+    {
+        $article = Article::factory(1)->create()->first();
+
+        $response = $this->get('/article/'.$article->slug);
+        $response->assertStatus(200);
+    }
+
+    public function testInvalidArticleReturnsNotFoundStatusCode()
+    {
+        $response = $this->get('/article/this-slug-should-not-be-found');
+        $response->assertStatus(404);
+    }
+}
