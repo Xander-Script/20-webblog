0fc9e3e3da1d8e5fa53cb94fcee39a1b6b820627
Xanderificnl <xander@web-refinery.com>
2021-11-26 21:59:59 +0100

    Add a small partial for going back

---
+++ resources/views/partials/previous-button.blade.php
@@ -0,0 +1,9 @@
+<div class="mb-4 mt-0 text-black">
+    <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-between">
+        <a href="{{ $url }}">
+            <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md">
+                « Previous
+            </span>
+        </a>
+    </nav>
+</div>
