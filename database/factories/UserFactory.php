1d4537be7d947c00a4828382f137a32901da59aa
Xanderificnl <xander@web-refinery.com>
2021-11-27 11:47:23 +0100

    Create factory users w/ the premium flag randomized

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,7 +12,7 @@ class UserFactory extends Factory
      *
      * @return array
      */
-    public function definition()
+    public function definition(): array
     {
         return [
             'name' => $this->faker->name(),
@@ -20,20 +20,32 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'premium' => $this->faker->boolean(),
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     * @return Factory
      */
-    public function unverified()
+    public function unverified(): Factory
     {
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
             ];
+        });
+    }
+
+    /**
+     * @param bool $premium
+     * @return Factory
+     */
+    public function premium(bool $premium = true): Factory
+    {
+        return $this->state(function (array $attributes) use ($premium) {
+            return ['premium' => $premium];
         });
     }
 }
