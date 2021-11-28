6c4e2e7f8bf38ec7cc3c4b8dbac69178ffeb6198
Xanderificnl <xander@web-refinery.com>
2021-11-28 19:16:47 +0100

    Fix (add) subscription link

--- resources/views/articles/post.blade.php
+++ resources/views/articles/post.blade.php
@@ -3,8 +3,7 @@
         <header class="mb-6">
             <h1 class="title">
                 @php
-                // TODO - fix subscribe link
-                $article->slug = $article->slug ? route('article.show', $article->slug) : '#subscribe';
+                $article->slug = $article->slug ? route('article.show', $article->slug) : route('subscribe');
                 @endphp
                 <a href="{{ $article->slug }}"
                    class="hover:text-gray-600 hover:font-bold">
@@ -20,7 +19,7 @@
             </div>
         </header>
 @if ($article->premium && ! Auth::userIsPremium())
-            <h2 class="text-2xl">This article is for paid subscribers only. <a href="#" class="font-semibold">Start your free trial now &raquo;</a></h2>
+            <h2 class="text-2xl">This article is for paid subscribers only. <a href="{{ route('subscribe') }}" class="font-semibold">Start your free trial now &raquo;</a></h2>
 @else
             {!! nl2br(e($article->body)) !!}
 
