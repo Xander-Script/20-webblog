3b55a02f551325e107d0ffd72cad624c6c6ff05a
Xanderificnl <xander@web-refinery.com>
2021-11-28 17:03:29 +0100

    Create an intermediate table for articles & categories

---
+++ database/migrations/2021_11_28_154902_create_article_category_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_category', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->foreignId('article_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('article_category', function (Blueprint $table) {
+            Schema::dropIfExists('article_category');
+        });
+    }
+}
