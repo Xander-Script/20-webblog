964ecc7535b0bd232d70d2153c94c5ca389e26d9
Xanderificnl <xander@web-refinery.com>
2021-11-30 15:06:50 +0100

    Update user seeder to create users to rbac with

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,7 +14,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        foreach (['root', 'editor', 'author', 'premium member', 'member'] as $role) {
+            $user = User::create([
+                'name' => "$role User",
+                'email' => str_replace(' ', '-', $role).'@example.org',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => null,
+            ]);
+
+            $user->assignRole($role);
+        }
         \App\Models\User::factory(10)->create();
-        //
     }
 }
