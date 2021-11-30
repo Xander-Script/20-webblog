ae14251cfce06baffca04648f7010bcd39d4c67f
Xanderificnl <xander@web-refinery.com>
2021-11-30 15:48:28 +0100

    Remove unneeded code and refactor CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,83 +10,12 @@ use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
+    public string $model = 'category';
+
     public function index(): View
     {
-        $categories = Category::withCount([
-            'articles as free_article_count' => function (Builder $query) {
-                $query->withoutGlobalScope('guest')->where('premium', 0);
-            },
-            'articles as premium_article_count' => function (Builder $query) {
-                $query->withoutGlobalScope('guest')->where('premium', 1);
-            },
-        ])->orderBy('name')->cursorPaginate();
+        $categories = Category::orderBy('name')->cursorPaginate();
 
         return view('category.index', ['categories' => $categories]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param Category $category
-     * @return Response
-     */
-    public function show(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param Category $category
-     * @return Response
-     */
-    public function edit(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param Category $category
-     * @return Response
-     */
-    public function update(Request $request, Category $category)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param Category $category
-     * @return Response
-     */
-    public function destroy(Category $category)
-    {
-        //
     }
 }
