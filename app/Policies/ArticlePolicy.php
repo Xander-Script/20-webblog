f719a7954dba359a1ec24cbda73a7c839594b449
Xanderificnl <xander@web-refinery.com>
2021-11-30 15:07:56 +0100

    Update articlepolicy

--- app/Policies/ArticlePolicy.php
+++ app/Policies/ArticlePolicy.php
@@ -5,90 +5,107 @@ namespace App\Policies;
 use App\Models\Article;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Database\Eloquent\Builder;
+use Symfony\Component\HttpFoundation\Response;
 
 class ArticlePolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
+    public function __construct()
     {
-        //
+        $user = request()->user() ?? new User;
+
+        if (! $user->hasRole(['root', 'editor', 'author', 'premium user'])) {
+            Article::addGlobalScope('guest', function (Builder $builder) {
+                $builder->where('premium', 0);
+            });
+        }
+
+        if (! $user->hasRole(['root', 'editor', 'author'])) {
+            Article::addGlobalScope('published', function (Builder $builder) {
+                $builder->where('draft', 0);
+            });
+        }
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Article  $article
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, Article $article)
+    // ArticleController::index
+    public function viewAny(?User $user): bool
     {
-        //
+        return true;
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
+    public function view(?User $user, Article $article): bool
     {
-        //
-    }
+        if ($article->premium) {
+            if ($user === null) {
+                abort(Response::HTTP_UNAUTHORIZED, 'Please sign in');
+            }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Article  $article
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, Article $article)
-    {
-        //
-    }
+            if (! $user->hasRole('premium user')) {
+                abort(Response::HTTP_PAYMENT_REQUIRED, 'Subscribe to view this article');
+            }
+        }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Article  $article
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, Article $article)
-    {
-        //
+        return true;
     }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Article  $article
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, Article $article)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Article  $article
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, Article $article)
-    {
-        //
-    }
+//
+//    /**
+//     * Determine whether the user can create models.
+//     *
+//     * @param User $user
+//     * @return Response|bool
+//     */
+//    public function create(User $user)
+//    {
+//        //
+//    }
+//
+//    /**
+//     * Determine whether the user can update the model.
+//     *
+//     * @param User $user
+//     * @param Article $article
+//     * @return Response|bool
+//     */
+//    public function update(User $user, Article $article)
+//    {
+//        //
+//    }
+//
+//    /**
+//     * Determine whether the user can delete the model.
+//     *
+//     * @param User $user
+//     * @param Article $article
+//     * @return Response|bool
+//     */
+//    public function delete(User $user, Article $article)
+//    {
+//        //
+//    }
+//
+//    /**
+//     * Determine whether the user can restore the model.
+//     *
+//     * @param User $user
+//     * @param Article $article
+//     * @return Response|bool
+//     */
+//    public function restore(User $user, Article $article)
+//    {
+//        //
+//    }
+//
+//    /**
+//     * Determine whether the user can permanently delete the model.
+//     *
+//     * @param User $user
+//     * @param Article $article
+//     * @return Response|bool
+//     */
+//    public function forceDelete(User $user, Article $article)
+//    {
+//        //
+//    }
 }
