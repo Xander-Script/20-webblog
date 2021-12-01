2681ce53c87bb22724c927540bd429b1555a0434
Xanderificnl <xander@web-refinery.com>
2021-12-01 13:20:26 +0100

    Fix link increasing in size due to font-bold.

--- resources/views/articles/post.blade.php
+++ resources/views/articles/post.blade.php
@@ -2,8 +2,7 @@
     <div class="p-6 border-b border-gray-200">
         <header class="mb-6">
             <h1 class="title">
-                <a href="{{ route('article.show', $article->slug) }}"
-                   class="hover:text-gray-600 hover:font-bold">
+                <a href="{{ route('article.show', $article->slug) }}">
                     {{ $article->title }}
                 </a>
             </h1>
