3e26315434e9c62d7d48075b0e239cc1d948f188
Xanderificnl <xander@web-refinery.com>
2021-12-04 14:05:27 +0100

    Improve article styling

--- resources/views/articles/_article.blade.php
+++ resources/views/articles/_article.blade.php
@@ -1,18 +1,19 @@
 <article itemscope itemtype="http://schema.org/BlogPosting" class="{{ !$article->premium ?: 'premium' }}">
+    @php($link = route('article.show', $article->slug))
     <header>
         <a href="{{ route('article.show', $article->slug) }}">
             <h1 itemprop="headline">{{ $article->title  }}</h1>
         </a>
 
         <div class="meta">
-            Published <time itemprop="datePublished" datetime="{{ $article->published_at->toIso8601String() }}">{{ $article->published_at->diffForHumans() }}</time>
-            <address>By <a href="#" rel="author">{{ $article->user->name }}</a></address>
+            <address><a href="#" rel="author">{{ $article->user->name }}</a></address>
+            on <time itemprop="datePublished" datetime="{{ $article->published_at->toIso8601String() }}">{{ $article->published_at->formatLocalized('%B %d, %Y') }}</time>
         </div>
-        <link href="{{ route('article.show', $article->slug) }}" itemprop="url">
+        <link href="{{ $link }}" itemprop="url">
     </header>
     @if (isset($type) && $type == 'description')
     <p itemprop="abstract">
-        {{ $article->description }}
+        {{ $article->description }} [...]
     </p>
     @else
         <p itemprop="articleBody">
@@ -20,7 +21,7 @@
         </p>
     @endif
     <footer>
-        <a class="read-more" href="{{ route('article.show', $article->slug) }}">{{ __("Read more") }} &raquo;</a>
+        <a class="read-more" href="{{ $link }}">{{ __("Read more") }} &raquo;</a>
 
         <div class="meta">
             <time itemprop="dateCreated" datetime="{{ $article->created_at->toIso8601String() }}">Created {{ $article->created_at->diffForHumans()}}</time>
