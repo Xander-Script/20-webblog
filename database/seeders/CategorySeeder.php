b7a2e6b43872297d18263c621430573bd28fdd17
Xanderificnl <xander@web-refinery.com>
2021-12-07 14:01:21 +0100

    Resolve a silly bug that aborts test seeds

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\QueryException;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -15,7 +16,10 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-//        Category::factory()->count(1)->fill(['name' => 'Uncategorized', 'id' => 1])->create();
-        Category::factory(20)->create();
+        try {
+            Category::factory(20)->create();
+        } catch (QueryException $e) {
+            // pass.
+        }
     }
 }
