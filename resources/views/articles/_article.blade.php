84ddd7ec2bd1343907a1049349eba4f80dd20309
Xanderificnl <xander@web-refinery.com>
2021-12-07 14:02:40 +0100

    Cleans up `_article.blade.php`

--- resources/views/articles/_article.blade.php
+++ resources/views/articles/_article.blade.php
@@ -1,68 +1,34 @@
 <article itemscope itemtype="https://schema.org/BlogPosting" class="{{ !$article->premium ?: 'premium' }}">
     <header>
-        <h1><a href="#" rel="author"><address>{{ $article->user->name }}</address></a></h1>
-        <h2><a href="{{ $article->link() }}" tabindex="1">{{ $article->title }}</a></h2>
+        <time itemprop="datePublished" datetime="{{ $article->published_at->toIso8601String() }}">
+            {{ $article->published_at->formatLocalized('%b %e, %Y') }}
+        </time>
+        <h1>
+                <a href="{{ $article->link() }}" tabindex="1">{{ $article->title }}</a>
+        </h1>
+        @if($article->premium && (!Auth::user() || Auth::user()->can('view', $article)))
+        <p class="premium-lead">
+            This is a premium article. In order to read more than just the summary, you'll need to <a
+                href="{{ route('login') }} ">sign in</a> or <a href="{{ route('subscribe') }}">subscribe</a>.
+        </p>
+        @endif
+        <div>
+            <address>
+                @if($authors)
+                    {{ $authors->firstWhere('id', $article->user_id)->name  }}
+                @else
+                    {{ $article->user->name }}
+                @endif
+            </address>
+            <p>writes:</p>
+        </div>
     </header>
     <div>
-        <time itemprop="datePublished" datetime="{{ $article->published_at->toIso8601String() }}">{{ $article->published_at->formatLocalized('%B %d, %Y') }} – </time> {{ $article->description }}
+
+        {{ $article->description }} [...]
     </div>
     <footer>
-        @if($article->premium)
-            <strong class="font-bold">premium article.</strong>
-        @endif
+        <a href="{{ $article->link() }}">Continue to article &raquo;</a>
+        {{ __("This article was last updated on :0", [$article->updated_at->diffForHumans()]) }}
     </footer>
 </article>
-
-<hr class="my-4">
-
-
-{{--<article itemscope itemtype="http://schema.org/BlogPosting" class="{{ !$article->premium ?: 'premium' }}">--}}
-{{--    <article>--}}
-{{--        <header>--}}
-{{--            <a href="{{ $article->link() }}">--}}
-{{--                <h1 itemprop="headline">{{ $article->title  }}</h1>--}}
-{{--            </a>--}}
-
-{{--            <div class="meta">--}}
-{{--                <address><a href="#" rel="author">{{ $article->user->name }}</a></address>--}}
-{{--                on <time itemprop="datePublished" datetime="{{ $article->published_at->toIso8601String() }}">{{ $article->published_at->formatLocalized('%B %d, %Y') }}</time>--}}
-{{--            </div>--}}
-{{--            <link href="{{ $article->link() }}" itemprop="url">--}}
-{{--            <h1><address><a href="#" rel="author">{{ $article->user->name }}</a></address> wrote</h1>--}}
-{{--            <h2>{{ $article->title }}</h2>--}}
-{{--        </header>--}}
-{{--        @if (isset($type) && $type == 'description')--}}
-{{--            <p itemprop="abstract">--}}
-{{--                {{ $article->description }} [...]--}}
-{{--            </p>--}}
-{{--        @else--}}
-{{--            <p itemprop="articleBody">--}}
-{{--                {{ $article->body }}--}}
-{{--            </p>--}}
-{{--        @endif--}}
-{{--        <footer>--}}
-{{--            <a class="read-more" href="{{ $article->link() }}">{{ __("Read more") }} &raquo;</a>--}}
-
-{{--            <div class="meta">--}}
-{{--                <time itemprop="dateCreated" datetime="{{ $article->created_at->toIso8601String() }}">Created {{ $article->created_at->diffForHumans()}}</time>--}}
-{{--                and--}}
-{{--                <time itemprop="dateModified" datetime="{{ $article->updated_at->toIso8601String() }}">last modified {{ $article->updated_at->diffForHumans() }}</time>--}}
-{{--            </div>--}}
-{{--            <div class="categories">--}}
-{{--                @forelse ($article->categories as $category)--}}
-{{--                    <a href="{{ route('category.show', $category->slug) }}">--}}
-{{--                        {{$category->name}}--}}
-{{--                    </a>--}}
-{{--                @empty--}}
-{{--                    --}}{{--                TODO--}}
-{{--                @endforelse--}}
-{{--            </div>--}}
-{{--        </footer>--}}
-{{--        <div>--}}
-{{--            <time itemprop="datePublished" datetime="{{ $article->published_at->toIso8601String() }}">{{ $article->published_at->formatLocalized('%B %d, %Y') }} &dash; </time> {{ $article->description }}--}}
-{{--        </div>--}}
-{{--        <footer></footer>--}}
-{{--    </article>--}}
-
-{{--    <hr class="my-4">--}}
-
