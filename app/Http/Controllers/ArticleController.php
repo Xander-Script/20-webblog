c5418238b3d7e02e859883c86116e27f51fa3cff
Xanderificnl <xander@web-refinery.com>
2021-12-08 15:16:46 +0100

    Clean up ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,38 +12,10 @@ class ArticleController extends Controller
 {
     public function index(): view
     {
-
-//        dd($x);
-//        die($this->)
-
         return $this->view([
             'authors' => Role::where('name', 'author')->first()->users()->get(),
             'categories' => Category::all(),
             'articles' => Article::latest('published_at')->without(['user', 'categories'])->paginate(5),
         ]);
     }
-
-//    public string $model = 'article';
-
-//    public string $model = 'article';
-//
-//    public int $itemsPerPage = 5;
-//
-//    public function index(): View
-//    {
-//
-//        // TODO cache...
-//        return view('articles.index', [
-//            'categories' => Category::all(['id', 'name', 'slug']),
-//            'authors' => Role::where('name', 'author')->first()->users()->get(),
-//            'articles' => Article::latest('published_at')->paginate($this->itemsPerPage),
-//        ]);
-//    }
-//
-//    public function show(Article $article): View
-//    {
-//        return view('articles.show', [
-//            'article' => $article,
-//        ]);
-//    }
 }
